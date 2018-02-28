00f2957b5c0b4aa4b885a7d5f5ffa612ff83e618
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-28 08:26:38 -0300

    inicial

--- src/Atendimento/Ajax/Views/clientes/apagados/index.blade.php
+++ src/Atendimento/Ajax/Views/clientes/apagados/index.blade.php
@@ -1,41 +1,55 @@
-@extends( Config::get('app.templateMaster' , 'templates.templateMaster')  )
+@extends( Config::get('app.templateMasterJson' , 'templates.templateMasterJson')  )
 
-@section( Config::get('app.templateMasterContentTitulo' , 'titulo-page')  )
-	Listagem dos Clientes			
-@endsection
-
-@section( Config::get('app.templateMasterMenuLateral' , 'menuLateral')  )	
-		<li><a href="{{  route('clientes.index')}}"><i class="fa fa-circle-o text-green"></i> <span>Clientes Ativos</span></a></li>
-@endsection
+@push('styles') 
+	<style>
+		.content-wrapper {	background-color:#ffc9c9;	}
+		.box , .box-footer{	background: #fee;	}
+	</style>
+@endpush
 
 @section( Config::get('app.templateMasterContent' , 'content')  )
-
-<div class="col-xs-12">
-    <div class="box box-success">	
-        <div class="box-body">
-            <table class="table table-bordered table-striped table-hover" id="datatable">
-                <thead>
-                    <tr>
-						<th>ID</th>
-						<th pesquisavel>Nome</th>						
-                        <th class="align-center" style="width:180px;">Ações</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-    </div>
-</div>
-
+<section class="content-header">
+	<h1>
+		<span id="div-titulo-pagina">
+			Listagem dos Clientes Apagados	
+		</span>
+	</h1>
+</section>	
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-success" id="div-box"> 
+				<div class="box-body" style="padding-top: 5px; padding-bottom: 3px;">
+					<table class="table table-bordered table-striped table-hover" id="datatable">
+						<thead>	
+							<tr>
+								<th>ID</th>
+								<th pesquisavel>Nome</th>						
+								<th class="align-center" style="width:180px;">Ações</th>
+							</tr>
+						</thead>
+					</table>
+				</div>     
+			</div>
+		</div>
+	</div>
+</section>
 @endsection
+
+
+
 
 @push(Config::get('app.templateMasterScript' , 'script') )
 	<script src="{{ mix('js/datatables-padrao.js') }}" type="text/javascript"></script>
+
 	<script>
-		$(document).ready(function() {
+
+		var pagianIndex = document.getElementById("div-pagina").innerHTML;		
+		function modelIndexDataTableFunction() {
 			var dataTable = datatablePadrao('#datatable', {
 				order: [[ 1, "asc" ]],
 				ajax: { 
-					url:'{{ route('clientes.getDatatable.apagados') }}'
+					url:'{{ route('clientes.ajax.apagados.getDatatable') }}'
 				},
 				columns: [
 					{ data: 'id', name: 'id' },
@@ -43,16 +57,42 @@
 					{ data: 'action', name: 'action', orderable: false, searchable: false, class: 'align-center'}
 				],
 			});
-
+	
 			dataTable.on('draw', function () {
 				$('[btn-excluir]').click(function (){
-					excluirRecursoPeloId($(this).data('id'), "@lang('msg.conf_excluir_o', ['1' => 'clientes'])", "{{ route('clientes.index') }}", 
+					excluirRecursoPeloId($(this).data('id'), "@lang('msg.conf_excluir_o', ['1' => 'Cliente' ])", "{{ route('clientes.ajax.apagados.index') }}", 
 						function(){
 							dataTable.row( $(this).parents('tr') ).remove().draw('page');
 						}
 					);
 				});
+
+				$('[btn-show]').click(function (){					
+					modelShow($(this).data('id'), "{{ route('clientes.ajax.apagados.index') }}",
+						function(data){							
+							document.getElementById("div-pagina").innerHTML = data ;						
+						}
+					);                 
+				});
+
+				$('[btn-restaurar]').click(function (){					
+					modelRestaurar($(this).data('id'), "{{ route('clientes.ajax.apagados.index') }}",
+						function(){							
+							dataTable.row( $(this).parents('tr') ).remove().draw('page');					
+						} 	
+					);                 
+				});
+
 			});
+
+		}
+
+
+		$(document).ready(function() {
+			modelIndexDataTableFunction();
 		});
 	</script>
+
+
+
 @endpush
